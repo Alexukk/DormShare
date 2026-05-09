922647a9b233616f601a384a18a44ccb28de5a56
Alex <alexukrok@gmail.com>
2026-05-09 12:40:40 +0300

    Implemented function for jwt token creation

--- DormShareAPI/application/Auth.py
+++ DormShareAPI/application/Auth.py
@@ -1,13 +1,20 @@
+from jwt.algorithms import Algorithm
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import jwt
 from typing import List, Optional
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from DormShareAPI.application.Data import models, DataBase
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+ACCESS_TOKEN_EXPIRE_HOURS = int(os.getenv("TOKEN_EXPIRES", 72))
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
 
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
@@ -20,3 +27,16 @@ def get_password_hash(password: str):
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    if "sub" in to_encode:
+        to_encode["sub"] = str(to_encode["sub"])
+
+    expire = datetime.now(timezone.utc) + timedelta(hours=ACCESS_TOKEN_EXPIRE_HOURS)
+    to_encode.update({"exp" : expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
